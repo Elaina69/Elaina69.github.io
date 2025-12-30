c24dd179ea21526f0c7f6f102ef01df661bbec89
Elaina69 <vipnoxhd123@gmail.com>
2025-12-30 14:02:01 +0100

    Update

--- configs/projects.js
+++ configs/projects.js
@@ -2,16 +2,9 @@ export default [
     {
         name: 'Elaina theme',
         description: 'One of the best theme for Pengu Loader - League of Legends.',
-        tags: ['JavaScript', "TypeScript", 'CSS', "Node.js", "ExpressJS", "MySQL"],
+        tags: ['JavaScript', "TypeScript", 'CSS', "Node.js", "ExpressJS", "MariaDB"],
         background: './assets/images/elaina-theme.webp',
         link: 'https://github.com/Elaina69/Elaina-V4'
-    },
-    {
-        name: 'Yes Steve Models Repo',
-        description: 'YSM models I collected from various sources, free to use.',
-        tags: ["YSM", "3D Models", "Community Project"],
-        background: './assets/images/ysm.webp',
-        link: 'https://github.com/Elaina69/Yes-Steve-Model-Repo'
     },
     {
         name: 'Auto ban bot',
@@ -19,5 +12,19 @@ export default [
         tags: ["Node.js", "Discord.js"],
         background: 'none',
         link: 'https://github.com/Elaina69/Auto-Ban'
+    },
+    {
+        name: 'Elaina web scripts',
+        description: 'My collection of bullshjt web scripts I made =.=!',
+        tags: ["JavaScript", "Tampermonkey"],
+        background: 'none',
+        link: 'https://github.com/Elaina69/Elaina-web-scipts'
+    },
+    {
+        name: 'Yes Steve Models Repo',
+        description: 'YSM models I collected from various sources, free to use.',
+        tags: ["YSM", "3D Models", "Community Project"],
+        background: './assets/images/ysm.webp',
+        link: 'https://github.com/Elaina69/Yes-Steve-Model-Repo'
     },
 ];
